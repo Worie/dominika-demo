0242f29e458d630b537f16136f30aa150420690b
Wojciech Połowniak <wojtek.polowniak@functionite.com>
2018-08-27 18:19:02 +0200

    add mouse integration

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('hi, there');
+
+const getRandomArbitrary = (min, max) => {
+  return Math.random() * (max - min) + min;
+}
+
+
+const handleMouseMove = () => {
+  let r,g,b,a;
+
+  r = getRandomArbitrary(0,255);
+  g = getRandomArbitrary(0,255);
+  b = getRandomArbitrary(0,255);
+  a = Math.random();
+
+  const randomColor = `rgba(${r},${g},${b},${a})`;
+  document.body.style.background = randomColor;
+};
+
+document.body.addEventListener('mousemove', handleMouseMove);
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('hi, there');
+});
